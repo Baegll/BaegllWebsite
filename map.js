ba9cfe4725a2573e88842bcd4352ce148754e44d
Jackson Johanek <jack.johanek@gmail.com>
2023-12-04 20:22:44 -0600

    Adding interactive map, and cleaning up useless comments

---
+++ map.js
@@ -0,0 +1,22 @@
+var map = L.map('map').setView([0, 0], 1); // Set the initial center and zoom level
+
+var imageUrl = 'img/Logo.png';
+var imageBounds = [[-90, -180], [90, 180]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+var customIcon1 = L.icon({
+    iconUrl: 'img/LogoTransparent.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [0, -16]
+});
+
+var customIcon2 = L.icon({
+    iconUrl: 'img/LogoTransparent.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [0, -16]
+});
+
+var marker1 = L.marker([51.505, -0.09], { icon: customIcon1 }).addTo(map).bindPopup('The Hilt');
+var marker2 = L.marker([40.7128, -74.0060], { icon: customIcon2 }).addTo(map).bindPopup('Inky Darkness');

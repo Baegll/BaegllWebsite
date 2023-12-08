4920566d00b4d906efa8c9a9a39a546e31f4107d
Jackson Johanek <jack.johanek@gmail.com>
2023-12-07 23:23:07 -0600

    Another terrible search

--- src/javascript/search.js
+++ src/javascript/search.js
@@ -7,8 +7,6 @@ var pageSuggestions = [
     { title: 'World Map', url: 'interactive-map.html', content: null},
 ];
 
-
-
 function showSuggestions() {
     var searchInput = document.getElementById('searchInput').value.toLowerCase();
     var suggestionContainer = document.getElementById('suggestionContainer');
@@ -38,41 +36,6 @@ function showSuggestions() {
     }
 }
 
-async function getContentParagraph(path) {
-    try {
-      const response = await fetch(path);
-  
-      if (!response.ok) {
-        throw new Error(`Failed to fetch ${path}: ${response.status} ${response.statusText}`);
-      }
-  
-      const htmlString = await response.text();
-      const paragraphs = extractParagraphs(htmlString);
-  
-      console.log(paragraphs);
-      return paragraphs;
-    } catch (err) {
-      console.warn('Something went wrong.', err);
-      throw err;
-    }
-}
-  
-function extractParagraphs(htmlString) {
-      var parser = new DOMParser();
-  
-      var doc = parser.parseFromString(htmlString, 'text/html');
-  
-      var paragraphs = doc.querySelectorAll('p');
-  
-      var paragraphTexts = Array.from(paragraphs).map(paragraph => paragraph.textContent);
-  
-      return paragraphTexts;
-}
-  
-async function fillPageSuggetion(title, page) {
-    const newContent = await getContentParagraph(page)
-    updateContentByTitle(title, newContent);
-}
 
 function updateContentByTitle(title, newContent) {
     var index = pageSuggestions.findIndex(page => page.title === title);
@@ -83,8 +46,16 @@ function updateContentByTitle(title, newContent) {
     } else {
       console.log(`Title not found: ${title}`);
     }
-  }
+}
 
-  
-fillPageSuggetion('index.html', 'https://baegll.github.io');
-fillPageSuggetion('lorem.html', 'https://baegll.github.io/lorem.html');
+function fetchLocalText(title, page) {
+    fetch(page)
+        .then((res) => res.text())
+        .then((text) => {
+            updateContentByTitle(title, text);
+        })
+        .catch((e) => console.error(e));
+}
+
+fetchLocalText('Main Page', 'index.html');
+fetchLocalText('Lorem', 'lorem.html');
